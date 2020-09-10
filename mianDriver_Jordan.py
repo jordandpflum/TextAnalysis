f6dddc5345e079e1171b1fb325f87832f6bd11c5
jordandpflum <jordandpflum@gmail.com>
2020-09-10 11:20:53 -0500

    Added Web Scraper - Jordan

--- mianDriver_Jordan.py
+++ mianDriver_Jordan.py
@@ -2,3 +2,8 @@ from webScraper import edmundWebScraper
 from replaceModelWBrand import replaceModelWBrand
 from MDS import MDS
 from calculateLift import calculateLift
+
+url = 'https://forums.edmunds.com/discussion/3941/general/x/i-spotted-a-new-insert-make-model-today'
+pages = 200
+comments = edmundWebScraper(url, pages)
+comments.to_csv("results.csv")
--- webScraper.py
+++ webScraper.py
@@ -1,10 +1,48 @@
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+import pandas as pd
+
+def edmundWebScraper(url, pages):
+    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    driver.get(url)
+    comments = pd.DataFrame(columns=['Date', 'user_id', 'comments'])
+    for i in range(pages):
+        print(i)
+        page_comments = scrapePageComments()
+        comments = comments.append(page_comments)
+        driver.find_element_by_class_name('Next').click()
+
+    return comments
 
 
-def edmundWebScraper():
+def scrapePageComments(driver):
+    page_comments = pd.DataFrame(columns=['Date', 'user_id', 'comments'])
+    ids = driver.find_elements_by_xpath("//*[contains(@id,'Comment_')]")
 
-    return None
+    comment_ids = []
+    for i in ids:
+        comment_ids.append(i.get_attribute('id'))
 
+    for x in comment_ids:
+        # Extract dates from for each user on a page
+        user_date = driver.find_elements_by_xpath('//*[@id="' + x + '"]/div/div[2]/div[2]/span[1]/a/time')[0]
+        date = user_date.get_attribute('title')
 
+        # Extract user ids from each user on a page
+        userid_element = driver.find_elements_by_xpath('//*[@id="' + x + '"]/div/div[2]/div[1]/span[1]/a[2]')[0]
+        userid = userid_element.text
+
+        # Extract Message for each user on a page
+        user_message = driver.find_elements_by_xpath('//*[@id="' + x + '"]/div/div[3]/div/div[1]')[0]
+        comment = user_message.text
+
+        # Adding date, userid and comment for each user in a dataframe
+        page_comments.loc[len(page_comments)] = [date, userid, comment]
+
+    return page_comments
 
 
 
