9e336877f4db0652087659c2241e33db1052bdd1
jordandpflum <jordandpflum@gmail.com>
2020-09-16 18:27:13 -0500

    Update replaceModelWBrand.py

--- replaceModelWBrand.py
+++ replaceModelWBrand.py
@@ -1,9 +1,21 @@
 
 
-def replaceModelWBrand():
-
-
-    return None
+def replaceModelWBrand(tokens, wordBrandCSV):
+    """
+    Replace Model Occurance with Brand
+    :param tokens: list of all important words in comments
+    :param wordBrandCSV: CSV of word-brand association
+    :return: a list of allBrands mentioned in the comments
+    """
+    brandWordAssociation = pd.read_csv('car models and brands.csv')
+    brandWords = brandWordAssociation['Model']
+    brandWordAssociation.set_index('Model')
+    brandWordAssociation = brandWordAssociation.to_dict()
+    for i in range(len(tokens)):
+        word = tokens[i].lower()
+        if word in brandWords:
+            tokens[i] = brandWordAssociation[word]
+    return tokens
 
 
 
